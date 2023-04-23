4434cea837489fb6b56735e55586feadce51a866
Olga Braiko <olga.braiko456@mail.ru>
2023-04-23 14:54:07 -0700

    tab

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
-console.log('Hello World!');
+import "./slider";
+import modals from "./modals";
+import tabs from "./tabs";
+window.addEventListener("DOMContentLoaded", () => {
+  modals();
+  tabs({
+    header: ".glazing_slider",
+    tab: ".glazing_block",
+    content: ".glazing_content",
+    activClass: "active",
+  });
+});
---
+++ src/js/modal.js
@@ -0,0 +1,50 @@
+const modals = () => {
+  const bindModal = (triggerSelector, modalSelector, closeSelector) => {
+    const triggers = document.querySelectorAll(triggerSelector);
+    const modal = document.querySelector(modalSelector);
+    const close = document.querySelector(closeSelector);
+
+    triggers.forEach((trigger) => {
+      trigger.addEventListener("click", (e) => {
+        if (e.target) {
+          e.preventDefault();
+        }
+        modal.style.display = "block";
+
+        document.body.classList.add("modal-open");
+      });
+    });
+
+    const closeModal = (close, modal) => {
+      close.addEventListener("click", () => {
+        modal.style.display = "none";
+        document.body.style.overflow = "";
+      });
+    };
+    modal.addEventListener("click", (e) => {
+      if (e.target === modal) {
+        modal.style.display = "none";
+        document.body.style.overflow = "";
+      }
+    });
+  };
+
+  closeModal();
+
+  const showModalByTime = (selector, time) => {
+    setTimeout(() => {
+      document.querySelector(selector).style.display = "block";
+      document.body.style.overflow = "hidden";
+    }, time);
+  };
+  showModalByTime(".popup", 60000);
+
+  bindModal({
+    trigger: ".popup_engineer_btn",
+    modal: ".popup_engineer",
+    close: ".popup_engineer .popup_close",
+  });
+  bindModal(".phone_link", ".popup", ".popup .popup_close");
+};
+
+export default modals;
