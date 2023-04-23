56aa07af10e715d2b5a45e99df74deab4b249acf
Olga Braiko <olga.braiko456@mail.ru>
2023-04-23 03:57:49 -0700

    popup

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,7 @@
-console.log('Hello World!');
+import "./slider";
+import modal from "./modal";
+//import tabs from "./tabs";
+window.addEventListener("DOMContentLoaded", () => {
+  modal();
+  //tabs(".glazing_slider", ".glazing_block", ".glazing_content", "active");
+});
