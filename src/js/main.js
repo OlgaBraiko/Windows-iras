d833cd87c9d09390b9d4a8d0f3af0ee1ac453705
Olga Braiko <olga.braiko456@mail.ru>
2023-05-09 09:06:34 -0700

    error

--- src/js/main.js
+++ src/js/main.js
@@ -2,18 +2,28 @@ import "./slider";
 import tabs from "./tabs";
 import modal from "./modal";
 import forms from "./forms";
+import changeModalState from "./changeModalState";
+import timer from "./timer";
+
 window.addEventListener("DOMContentLoaded", () => {
+  const modalState = {};
+  const deadline = "2023-05-12";
+
   tabs({
     header: ".glazing_slider",
     tabs: ".glazing_block",
     contents: ".glazing_content",
     activeClass: "active",
   });
-});
-
-window.addEventListener("DOMContentLoaded", () => {
+  tabs({
+    header: ".balcon_icons",
+    tabs: ".balcon_icons_img",
+    contents: ".big_img > img",
+    activeClass: "do_image_more",
+    display: "inline-block",
+  });
   modal();
-});
-window.addEventListener("DOMContentLoaded", () => {
   forms();
+  changeModalState(modalState);
+  timer(".container1", deadline);
 });
